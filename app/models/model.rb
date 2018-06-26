495440ec81a5c1b08e2c0577525fd8f3464d7619
Abigail Slovick <abby.slovick@gmail.com>
2018-06-26 15:06:19 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
 class Dog
-  # Replace with Dog class
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
+
+  def self.all
+    DOGS
+  end
+
 end
